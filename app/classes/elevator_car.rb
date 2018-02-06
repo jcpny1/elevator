3fb275842285e1dd985035c34762d7e3f048e188
John Pfingst <jpfingst@outlook.com>
2018-02-06 12:32:29 -0500

    Add queue between controller and elevator car threads.

--- app/classes/elevator_car.rb
+++ app/classes/elevator_car.rb
@@ -1,10 +1,26 @@
 # An ElevatorCar moves people between floors of a building.
 class ElevatorCar
 
-  def initialize
+  def initialize(command_q)
+    @command_q = command_q
     @direction = 'up'
     @door_status = 'closed'
     @location = 1
+    puts 'New Car'
+  end
+
+  def run
+    while e = @command_q.deq # wait for nil to break loop
+      dest = e[:floor].to_i
+      puts '<' + e[:cmd] + ' ' + e[:floor] + '>'
+      case e[:cmd]
+      when 'CALL'
+        goto e[:floor].to_i
+      when 'GOTO'
+        goto e[:floor].to_i
+      end
+    end
+    puts 'New Car' + ' thread done'
   end
 
   def door_close
@@ -12,8 +28,8 @@ class ElevatorCar
       puts '<door closing>'
       sleep 2
       @door_status = 'closed'
+      puts 'door ' + @door_status
     end
-    puts 'door ' + @door_status
   end
 
   def door_open
@@ -21,18 +37,30 @@ class ElevatorCar
       puts '<door opening>'
       sleep 2
       @door_status = 'open'
+      puts 'door ' + @door_status
     end
-    puts 'door ' + @door_status
+  end
+
+  def start
+    puts '<starting>'
+    sleep 0.25
+  end
+
+  def stop
+    puts '<stopping>'
+    sleep 1
   end
 
   def goto(dest)
     @direction = dest > @location ? 'up' : 'down'
     door_close
+    start
     while @location != dest
       sleep 1
       @direction === 'up' ? @location += 1 : @location -= 1
       puts "floor #{@location}"
     end
+    stop
     door_open
   end
 end
--- elevator.rb
+++ elevator.rb
@@ -1,39 +1,35 @@
 require_relative 'app/classes/elevator_car'
 
+command_q = Queue.new
 
-q = Queue.new
-threads = []
+controller_t = Thread.new('controller') { |name|
+  elevator_q = Queue.new
+  elevator_t = Thread.new('elevator') { |name|
+    elevator = ElevatorCar.new(elevator_q).run
+  }
 
-threads << Thread.new('elevator') { |name|
-  elevator = ElevatorCar.new
-  while e = q.deq # wait for nil to break loop
-    dest = e[:floor].to_i
-    puts '<' + e[:cmd] + ' ' + e[:floor] + '>'
-    case e[:cmd]
-    when 'GOTO'
-      elevator.goto(e[:floor].to_i)
-    end
+  while e = command_q.deq # wait for nil to break loop
+    elevator_q << e
   end
   puts name + ' thread done'
+# close elevator threads
 }
 
-threads << Thread.new('command') { |name|
-  puts 'GOTO 6'
-  q << {cmd: 'GOTO', floor: '6'}
-  sleep 15
-  puts 'GOTO 4'
-  q << {cmd: 'GOTO', floor: '4'}
-  puts 'CALL 2 UP'
-  q << {cmd: 'CALL', floor: '2', direction: 'up'}
-  q << nil
+command_t = Thread.new('command') { |name|
+sleep 1
+  puts '  GOTO 6'
+  command_q << {cmd: 'GOTO', floor: '6'}
+sleep 5
+  puts '  GOTO 4'
+  command_q << {cmd: 'GOTO', floor: '4'}
+sleep 5
+  puts '  CALL 2 UP'
+  command_q << {cmd: 'CALL', floor: '2', direction: 'up'}
+sleep 5
+  puts '  END'
+  command_q << nil
 }
 
-threads.each { |thr| thr.join }
-q.close
-
-# case ARGV[0]
-# when 'fib'
-#   puts rt.fib(ARGV[1].to_i)
-# else
-#   puts 'Invalid option'
-# end
+controller_t.join()
+command_t.join()
+command_q.close
