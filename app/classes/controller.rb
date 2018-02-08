8cd4af4ea613d0a0224f95c2d3f1f137e4b89e79
John Pfingst <jpfingst@outlook.com>
2018-02-07 22:47:28 -0500

    Standardize time measurement and messaging. Add elevator array and simple elevator selection.

--- app/classes/controller.rb
+++ app/classes/controller.rb
@@ -1,13 +1,39 @@
 # A Controller receives requests for elevator services and assigns elevators to service those requests.
 class Controller
 
-  @@simulation_time = 0  # seconds
+  NUM_ELEVATORS = 1
+
+  @@simulation_time = 0.0  # seconds
 
   def initialize(request_q)
     @request_q = request_q
-    @elevator_q = Queue.new
-    @elevator_t = Thread.new('elevator') do |name|
-      @elevator = ElevatorCar.new(@elevator_q).run
+    @elevators = []
+
+    NUM_ELEVATORS.times do |i|
+      e_queue  = Queue.new
+      e_thread = Thread.new("Elevator #{i}") { |name| ElevatorCar.new(name, e_queue).run }
+      @elevators << { queue: e_queue, thread: e_thread }
+    end
+  end
+
+  def run
+    while 1
+      if @request_q.length > 0
+        e = @request_q.deq
+        elevator = select_elevator(e)
+        elevator[:queue] << e
+      end
+# TODO we will be interpretting the command in a future release. So we'll be commanding elevator to terminate.
+#      for now, we'll just check the status of the first elevator to see if it was commanded to terminate.
+      break if !@elevators[0][:thread].status
+      sleep 0.25
+      @@simulation_time += 1.0
+    end
+
+    puts "Controller done. Simulation time: #{@@simulation_time}"
+    @elevators.each do |elevator|
+      elevator[:thread].join()
+      elevator[:queue].close
     end
   end
 
@@ -15,18 +41,9 @@ class Controller
     @@simulation_time
   end
 
-  def run
-    while 1
-      if @request_q.length > 0
-        e = @request_q.deq
-        @elevator_q << e
-      end
-      break if !@elevator_t.status
-      sleep 0.25
-      @@simulation_time += 1
-    end
-    puts "Controller done. Simulation time: #{@@simulation_time}"
-    @elevator_t.join()
-    @elevator_q.close
+private
+
+  def select_elevator(e)
+    @elevators[0]
   end
 end
--- app/classes/elevator_car.rb
+++ app/classes/elevator_car.rb
@@ -2,11 +2,11 @@
 class ElevatorCar
 
   FEET_PER_FLOOR  = 12.0
-  FEET_PER_SECOND =  5.0
+  FEET_PER_SECOND =  4.0
 
-  def initialize(command_q)
+  def initialize(name, controller_q)
     @car_status = 'holding'
-    @command_q = command_q
+    @controller_q = controller_q
     @current_direction = ''
     @current_location = 1
     @destinations = []  # floors to visit ordered by visit order.
@@ -20,31 +20,32 @@ class ElevatorCar
   def run
     drain_queue = false
     while 1
-      if Controller::time >= @next_command_time
-        if @command_q.length > 0
-          e = @command_q.deq # wait for nil to break loop
-          if e.nil?
-            drain_queue = true
+      # Check controller for incoming commands.
+      if @controller_q.length > 0
+        e = @controller_q.deq # wait for nil to break loop
+        if e.nil?
+          drain_queue = true
+        else
+          case e[:cmd]
+          when 'CALL', 'GOTO'
+            floor = e[:floor].to_i
+            @destinations << floor
+            @passengers[floor][:pickup] += e[:pickup].length
+            e[:pickup].each { |dest_floor| @passengers[dest_floor][:discharge] += 1 }
           else
-            case e[:cmd]
-            when 'CALL', 'GOTO'
-              floor = e[:floor].to_i
-              @destinations << floor
-              @passengers[floor][:pickup] += e[:pickup].length
-              e[:pickup].each { |dest_floor| @passengers[dest_floor][:discharge] += 1 }
-            else
-              puts '***Unknown command***'
-            end
+            puts '***Unknown command***'
           end
         end
-
+      end
+      # Execute next command.
+      if Controller::time >= @next_command_time
         if @destinations.length > 0
           car_move(@destinations[0] <=> @current_location)
         elsif drain_queue && @car_status === 'holding'
           door_close
           break;
         else
-          @next_command_time = Controller::time  # ready for next command
+          advance_next_command_time(0)  # ready for next command
         end
       end
       sleep 0.25
@@ -54,66 +55,77 @@ class ElevatorCar
 
 private
 
+  # Advance the time the given amount.
+  def advance_next_command_time(num)
+    @next_command_time += num
+  end
+
   # doors will be open 3 seconds per passenger on or off with a minimum open of 3 seconds.
-  def car_arrive
-    car_stop
-    door_open
+  def car_arrival
+    execute_command { car_stop }
+    execute_command { door_open }
 puts "<Discharge #{@passengers[@current_location][:discharge]}"
 puts "<Pickup    #{@passengers[@current_location][:pickup]}"
     passenger_time = (@passengers[@current_location][:discharge] * 3.0) + (@passengers[@current_location][:pickup] * 3.0)
     @passengers[@current_location][:discharge] = 0
     @passengers[@current_location][:pickup]    = 0
-    @next_command_time += passenger_time > 0.0 ? passenger_time : 3.0
+    advance_next_command_time(passenger_time > 0.0 ? passenger_time : 3.0)
   end
 
   # Move number of floors indicated. - = down, + = up, 0 = arrived.
   def car_move(floors)
-    if floors != 0
-      car_start
+    if floors == 0
+      execute_command { car_arrival }
+      @destinations.shift
+    else
+      execute_command { car_start }
       @current_direction = floors < 0 ? 'dn' : 'up'
       @current_location += floors
       @feet_traveled += floors.abs * FEET_PER_FLOOR
-      @next_command_time += floors.abs * FEET_PER_FLOOR/FEET_PER_SECOND
       puts "<floor #{@current_location}>"
-    else
-      car_arrive
-      @destinations.shift
+      advance_next_command_time(floors.abs * (FEET_PER_FLOOR/FEET_PER_SECOND))
     end
   end
 
   def car_start
     if @car_status === 'holding'
-      door_close
+      execute_command { door_close }
       puts '<starting>'
       @car_status = 'moving'
-      @next_command_time += 0.25
+      advance_next_command_time(0.25)
     end
   end
 
   def car_stop
     if @car_status === 'moving'
-      puts "<stopping on #{@current_location}>"
-puts "Simulation Time: #{Controller::time}"
+      puts "<stopping>"
       @car_status = 'holding'
-      @next_command_time += 1.0
+      puts "<stopped on #{@current_location}>"
+      advance_next_command_time(1.0)
     end
   end
 
   def door_close
     if @door_status != 'closed'
       puts '<door closing>'
-      @next_command_time += 2.0
       @door_status = 'closed'
       puts "<door #{@door_status}>"
+      advance_next_command_time(2.0)
     end
   end
 
   def door_open
     if @door_status != 'open'
       puts '<door opening>'
-      @next_command_time += 2.0
       @door_status = 'open'
       puts "<door #{@door_status}>"
+      advance_next_command_time(2.0)
     end
+  end
+
+  def execute_command
+    sleep 0.25 until Controller::time >= @next_command_time
+    yield
+    puts "Simulation Time: #{@next_command_time}"
   end
 end
